a4904d596d20a6c7e2ab8c61dc718b6ab9bc18ec
MarvinLiam2001 <54190422+MarvinLiam2001@users.noreply.github.com>
2020-03-23 20:16:02 +0100

    update

---
+++ Organisazija/healthkey.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+mp.events.add ('Verbandskasten', (player) => {
+    	
+        player.health = 100;
+   
+});
+
+mp.events.add ('Weste', (player) => {
+    	
+        player.armour = 100;
+   
+});
--- Organisazija/help.js
+++ Organisazija/help.js
@@ -5,9 +5,9 @@ let red = `#ff0000`;
 let white = `#ffffff`;
 let orange = `#f09500`;
 mp.events.addCommand("help", (player, message) => {
-	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${white}} /kleidung`);
+	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${white}} Commands: /kleidung`);
 });
 
 mp.events.addCommand("kleidung", (player, message) => {
-	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${orange}}[temporary-feature]!{${white}} /lcnkleidung, /orgkleidung, /yakuzakleidung`);
+	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${orange}} [temporary-feature]!{${white}} /lcnkleidung, /orgkleidung, /yakuzakleidung`);
 });
