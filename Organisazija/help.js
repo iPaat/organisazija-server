26ff8f60d83d5ce323a316fafeb812bb3a3ab392
MarvinLiam2001 <54190422+MarvinLiam2001@users.noreply.github.com>
2020-03-23 20:08:40 +0100

    addscripts

---
+++ Organisazija/help.js
@@ -0,0 +1,13 @@
+// Help Command //
+// !{${farbe}}
+
+let red = `#ff0000`;
+let white = `#ffffff`;
+let orange = `#f09500`;
+mp.events.addCommand("help", (player, message) => {
+	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${white}} /kleidung`);
+});
+
+mp.events.addCommand("kleidung", (player, message) => {
+	player.outputChatBox(`!{${red}}[Organisazija-Gangwar]!{${orange}}[temporary-feature]!{${white}} /lcnkleidung, /orgkleidung, /yakuzakleidung`);
+});
