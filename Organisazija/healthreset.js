f302554107b64a6e9f836ca4d8e177046fc5c6b1
MarvinLiam2001 <54190422+MarvinLiam2001@users.noreply.github.com>
2020-03-23 19:58:08 +0100

    helpupdate

--- Organisazija/healthreset.js
+++
@@ -1,7 +0,0 @@
-let playerHealth = player.health
-player.health = 100
-
-mp.events.addCommand("healthreset", (player) => {
-	let playerHealth = player.health
-    player.health = 100
-});
--- Organisazija/index.js
+++ Organisazija/index.js
@@ -1,3 +1,2 @@
 require('./clothes');
 require('./help');
-require('./healthreset');
